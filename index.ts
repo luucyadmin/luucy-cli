f2ad3511a710bc140bd7748106e18012c9c98489
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2021-12-07 15:30:47 +0100

    add default output / help

--- index.ts
+++ index.ts
@@ -9,21 +9,23 @@ const tar = require("tar");
 
 const action = process.argv.reverse().find(arg => arg[0] != "-");
 
+const environments = {
+    test: "http://localhost:4200",
+    staging: "https://staging.luucy.ch",
+    productive: "https://luucy.ch"
+};
+
 switch (action) {
     case "create": {
         const creator = new Creator();
         creator.create();
 
         process.exit(0);
+
+        break;
     }
 
     case "serve": {
-        const environments = {
-            test: "http://localhost:4200",
-            staging: "https://staging.luucy.ch",
-            productive: "https://luucy.ch"
-        };
-
         let host = environments.productive;
 
         for (let environment in environments) {
@@ -43,5 +45,52 @@ switch (action) {
         publisher.publish().then(() => {
             process.exit(0);
         });
+
+        break;
     }
+
+    default: {
+        const packageConfiguration = require("../package.json");
+
+        process.stdout.write(`luucy cli v${packageConfiguration.version}\n\n`);
+
+        const options = [
+            { 
+                name: "luucy create", 
+                purpose: "Creates an empty plugin locally" 
+            },
+            { 
+                name: "luucy serve", 
+                purpose: "Debug plugin locally",
+                arguments: Object.keys(environments).map(environment => ({
+                    name: `--${environment}`,
+                    purpose: `Launch debugger for ${environments[environment]} (${environment} environment)`
+                }))
+            },
+            { 
+                name: "luucy publish", 
+                purpose: "Publish a plugin to the luucy marketplace" 
+            }
+        ];
+
+        let longest = 0;
+
+        for (let option of options) {
+            longest = Math.max(longest, option.name.length);
+
+            for (let argument of option.arguments || []) {
+                longest = Math.max(longest, argument.name.length + 4);
+            }
+        }
+
+        longest += 3;
+
+        for (let option of options) {
+            process.stdout.write(`${option.name.padStart(longest)}${option.purpose}\n`);
+
+            for (let argument of option.arguments || []) {
+                process.stdout.write(`    ${argument.name.padStart(longest - 4)}${argument.purpose}\n`);
+            }
+        }
+    }    
 }
