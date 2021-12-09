984b0fbcde1e46891ba1d683a96139f1b26eea6c
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2021-12-09 15:42:17 +0100

    add luucy update

--- constants.ts
+++ constants.ts
@@ -2,4 +2,6 @@ export class Constants {
     static distFile = "dist.lps";
     static assetsDirectory = "assets";
     static iconFile = "assets/icon.svg";
+
+    static typesPackage = "luucy-types";
 }
