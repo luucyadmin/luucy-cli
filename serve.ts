07aa0dc6fe85c78823852b7969862449cc4e4176
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-02-10 11:21:57 +0100

    add esc replace

--- serve.ts
+++ serve.ts
@@ -95,9 +95,8 @@ export class Serve {
             process.stdout.write(data
                 .toString()
                 .replace(/([0-9][0-9]:?){3}\s+-\s+((Starting compilation in watch mode)|(File change detected. Starting incremental compilation))\.\.\./g, '')
-                .replace(/([0-9][0-9]:?){3}\s+-\s+Found (0-9)+ errors. Watching for file changes\./g, '')
-
-                + '*****'
+                .replace(/([0-9][0-9]:?){3}\s+-\s+Found [0-9]+ errors. Watching for file changes\./g, '')
+                .replace(/\x1b/g, 'ESC')
             );
         });
 
