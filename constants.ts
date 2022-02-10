2dba0fb92b193ba4ffea4967189f296b4402fa01
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-02-10 07:03:36 +0100

    add .luucy bundling

--- constants.ts
+++ constants.ts
@@ -1,13 +1,20 @@
+const path = require('path');
+
 export class Constants {
-    static distFile = "dist.lps";
-    static packageFile = "package.json";
-    static assetsDirectory = "assets";
-    static iconFile = "assets/icon.svg";
+    static distFile = 'dist.lps';
+    static packageFile = 'package.json';
+    static assetsDirectory = 'assets';
+    static iconFile = path.join(this.assetsDirectory, 'icon.svg');
 
-    static bundlesDirectory = "bundles/";
-    static bundleName = (name, version) => `${name.toLowerCase().trim().replace(/[^a-z0-9]/, "-").replace(/\-+/, "-")}-${version}.lpb`;
+    static bundlesDirectory = 'bundles/';
+    static bundleName = (name, version) => `${name.toLowerCase().trim().replace(/[^a-z0-9]/, '-').replace(/\-+/, '-')}-${version}.lpb`;
 
-    static typesPackage = "luucy-types";
+    static typesPackage = 'luucy-types';
+    static typesRoot = path.join('node_modules', this.typesPackage);
+    static scopes = path.join(this.typesRoot, 'scopes');
+
+    static managed = '.luucy';
+    static managedTypes = path.join(this.managed, 'types.d.ts');
 
     static environments = {
         test: 'http://localhost:4200',
