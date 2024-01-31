74831e0b5e47e26a3546673783d10f822df1bf1e
Thorben Westerhuys <thorben.westerhuys@luucy.ch>
2024-01-31 13:33:47 +0100

    require minimum of 60% coverage for CI to pass test step

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,22 @@
-import type { Config } from "jest";
+import type { Config } from 'jest';
 
 const config: Config = {
-  preset: "ts-jest",
+  preset: 'ts-jest',
   transform: {
-    "^.+\\.(ts|tsx)?$": "ts-jest",
+    '^.+\\.(ts|tsx)?$': 'ts-jest'
   },
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
-  coverageReporters: ["cobertura", "lcov", "text"],
-  reporters: ["default", ["jest-junit", { outputDirectory: "reports" }]],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageReporters: ['cobertura', 'lcov', 'text'],
+  reporters: ['default', ['jest-junit', { outputDirectory: 'reports' }]],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60
+    }
+  }
 };
 
 export default config;
