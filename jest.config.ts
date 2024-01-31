78143e1f955008b88ae3bc16caa8d041d5e4a252
Thorben Westerhuys <thorben.westerhuys@luucy.ch>
2024-01-31 09:10:55 +0100

    Add jest runner and initial coverage reporting

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/node_modules/**",
+    "!**/*.config.{js,ts,tsx}",
+  ],
+  coverageReporters: ["cobertura", "lcov", "text"],
+};
+
+export default config;
